fca2e6355abc8aea2f831f8e3d0d02dfd160d55d
Sanket <lakhera.sanket850@gmail.com>
2023-10-27 03:25:43 +0530

    code splitting done

---
+++ src/matching_engine/engine.rs
@@ -0,0 +1,54 @@
+use super::orderbook::{Order, Orderbook};
+use std::{collections::HashMap, fmt::format};
+// BTC/USD => BTC = BASE AND USD = QUOTE
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+pub struct TradingPair {
+    base: String,
+    quote: String,
+}
+
+impl TradingPair {
+    pub fn new(base: String, quote: String) -> TradingPair {
+        TradingPair { base, quote }
+    }
+
+    pub fn to_string(self) -> String {
+        format!("{}_{}", self.base, self.quote)
+    }
+}
+
+pub struct MatchingEngine {
+    orderbooks: HashMap<TradingPair, Orderbook>,
+}
+
+impl MatchingEngine {
+    pub fn new() -> MatchingEngine {
+        MatchingEngine {
+            orderbooks: HashMap::new(),
+        }
+    }
+
+    pub fn add_new_market(&mut self, pair: TradingPair) {
+        self.orderbooks.insert(pair.clone(), Orderbook::new());
+        println!("open new orderbook for market {:?}", pair.to_string())
+    }
+
+    pub fn place_limit_order(
+        &mut self,
+        pair: TradingPair,
+        price: f64,
+        order: Order,
+    ) -> Result<(), String> {
+        match self.orderbooks.get_mut(&pair) {
+            Some(orderbook) => {
+                orderbook.add_order(price, order);
+                println!("Placed limit order at price level {}", price);
+                Ok(())
+            }
+            None => Err(format!(
+                "The orderbook for the given trading pair ({}) does not exist",
+                pair.to_string()
+            )),
+        }
+    }
+}
